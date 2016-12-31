1dea0744d78b3fd652a5337505abac0126784140
raghuma sama <raghuma sama>
2016-12-31 10:41:59 -0500

    Test passed to convert 1066 into roman numerals

--- NumberToRoman.Library/NumberToRoman.cs
+++ NumberToRoman.Library/NumberToRoman.cs
@@ -11,6 +11,51 @@ namespace NumberToRoman.Library
         public static string GetRoman(int num)
         {
             string val = "";
+            while (num >= 1000)
+            {
+                val += "M";
+                num -= 1000;
+            }
+            if (num >= 900)
+            {
+                val += "CM";
+                num -= 900;
+            }
+            while (num >= 500)
+            {
+                val += "D";
+                num -= 500;
+            }
+            if (num >= 400)
+            {
+                val += "CD";
+                num -= 400;
+            }
+            while (num >= 100)
+            {
+                val += "C";
+                num -= 100;
+            }
+            if (num >= 90)
+            {
+                val += "XC";
+                num -= (0);
+            }
+            while (num >= 50)
+            {
+                val += "L";
+                num -= 50;
+            }
+            if (num >= 40)
+            {
+                val += "XL";
+                num -= 40;
+            }
+            while (num >= 10)
+            {
+                val += "X";
+                num -= 10;
+            }
             if (num >= 9)
             {
                 val += "IX";
