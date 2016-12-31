2207eb39394a3b98f2fcfc021b6e5b982b899b69
raghuma sama <raghuma sama>
2016-12-31 11:36:47 -0500

    refactored code

--- NumberToRomanConsole/Program.cs
+++ NumberToRomanConsole/Program.cs
@@ -3,13 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using lib=NumberToRoman.Library.NumberToRoman;
 namespace NumberRoman
 {
     class Program
     {
         static void Main(string[] args)
         {
+            string continuee;
+
+            do
+
+            {
+                Console.WriteLine("Please Enter the Arabic number to convert into Roman Numeral");
+            int GivenNumber = Convert.ToInt32(Console.ReadLine());
+            if (GivenNumber>=4000 || GivenNumber<=0)
+            {
+                Console.WriteLine("Please enter a number between 0 and 4000");
+            }
+            else
+            {
+                Console.WriteLine(lib.GetRoman(GivenNumber));
+            }
+                Console.WriteLine("do you want to covert other number(yes/no)");
+
+                continuee = Console.ReadLine();
+
+            }
+
+            while (continuee == "yes");
+
         }
     }
 }
